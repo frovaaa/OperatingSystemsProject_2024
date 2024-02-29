d32224cc0f23abcd2fc9d2fdfc6009eaca0a0529
costanza <rodric@usi.ch>
2024-02-29 17:45:56 +0100

    added sleepy_thread.h

---
+++ pintos/threads/sleepy_thread.h
@@ -0,0 +1,17 @@
+
+#ifndef SLEEPY_THREADS
+#define SLEEPY_THREADS
+
+#include "../lib/kernel/list.h"
+
+struct sleepy_thread {
+    struct list_elem elem;
+
+    struct thread * thread_p;
+
+    // awake_time represents the time in ticks of when the thread needs to wake up
+    int64_t awake_time;
+};
+
+
+#endif
