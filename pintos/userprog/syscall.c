d127c7797bed3ed604a62749331089d271f759b6
costanza <rodric@usi.ch>
2024-04-16 15:20:10 +0200

    first two tasks possibly done

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -15,6 +15,34 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  void * esp = f->esp + sizeof(enum syscall_code);
+  enum syscall_code s_code = *(enum syscall_code *)esp;
+
+  switch (s_code)
+  {
+  case SYS_WRITE:
+    esp += sizeof(int);
+    int fd = *(int *)(esp);
+    esp += sizeof(void *);
+    void * buffer = (void *)(esp);
+    esp += sizeof(size_t);
+    size_t size = *(size_t *)(esp);
+    putbuf(buffer, size);
+    break;
+  
+  case SYS_EXIT:
+    esp += sizeof(int);
+    int status = *(int *)(esp);
+    f->eax = status;
+    thread_exit ();
+    NOT_REACHED (); // as seen in thread.c, panic if thread cannot exit
+    break;
+
+  default:
+    printf("Unknown system call\n");
+    thread_exit ();
+    NOT_REACHED ();
+  }
 }
+
+
